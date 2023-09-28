aed9b4d440afe25425825d58201f8bf8beadc4d5
claude gauthier <claude_r_gauthier@hotmail.com>
2023-09-28 13:55:29 -0400

    first commit

---
+++ Readme.md
@@ -0,0 +1,96 @@
+# Performance Demo
+
+## Link to Live Demo
+
+http://www.claudegauthier.net/demos/performance/index.html
+
+
+## Introduction
+
+The purpose of this demo is to demonstrate that in certain cases, functional programming in JS is not always going to achieve high levels of performance while processing embedded loops.
+
+First of all, not all JS engines are identical and as such profiling should always be compared across the intended supported user agents.
+
+When there is processing involve in loops involving arrow based functions, then the optimization one would expect from the "one-liner" so to speak is effectively gone if this arrow function is embedded with more code functionality.
+
+## Performance as a requirement
+
+When performance is a required key metric, we must do our best to implement code that will deliver based on the expections set.
+
+In the best of all worlds, we should write code for clarity, consistency and scalability.
+
+Code that is well written is often also easy to unit test.
+
+As such we do our best to follow best practices and enforce clear design patterns.
+
+But...
+
+There are times when we must write code where performance goals supercede everything else.
+
+When a metric isn't met, we must look for alternatives.
+
+## What was I trying to solve?
+
+The demo code published in this repository is based on a real world problem I was faced.
+
+I needed to load a huge file that would allow me to view/edit a JSON document where the data is organized by Chapter, Policy, Section and Subsection.
+
+The purpose is to create a viewer/editor where the content is controlled in a granular manner akin to data manipulation and yet the editing is akin to word processing.
+
+Being able to achieve this goal would allow the tagging of various attributes with the purpose of being able to assign roles and entitlements and access to any part of the document.
+
+The JSON file that needed to be loaded contained nearly 23000 lines of code.
+
+The end result of the document loaded in the viewer was 3 prefaces in one section and 52 chapters with policies which contained sections, which contained subsections.
+
+In essence if it were to be printed, close to 400 pages of material.
+
+I developed a technique to allow real-time manipulation live in browser for the entire document.
+
+Everything loaded into memory.
+
+One of the reasons for this is that "numbering" had to be dynamically done.
+
+Chapter 1
+Policy 100
+Section 100.1
+Subsection 100.1.1
+
+New content created needs to be numbered on-the-fly, all other content has to quickly renumber itself.
+
+And moving content around or deleting content also required dynamic renumbering or a process to force renumbering.
+
+Suffice to say, first we must load the document.
+
+When I first implemented by embedded loops using Array.map(arrow function), the performance was atrociously slow.
+
+Given a high performance gaming system, it took over 10 seconds to load and render the document.
+
+Once I started to optimize the loops, going into a more traditional for loop format, for my code, I was able to load the and render the entire document in about 4 seconds.
+
+Thats'a huge performance gain.
+
+In this particular case, the optimization, while "ugly" in terms of code, obviously is the winner.
+
+## In the real world - What else could we do?
+
+The UI should do the least amount of processing possible.  We should try and offload large data processing to services.
+
+And with a bit of tweaking, I will eventually do that.
+
+But if I leave all of this in the front-end, I can also easily implement "Web Workers" as well.  Since there is no DOM manipulation, this is all computational, the processing would also increase. "So I suspect, I would need to profile! :)"
+
+The JSON being generated is added via the "initComponent" funciton of the viewer , which means that the layout system in ExtJS take overs and generates the view.
+
+
+## In conclusion
+
+The demo you have access doesn't do much, some pre-calculated messages stuffed into arrays to be output in the debugger's console in a processing manner identical to the code I wrote.
+
+But it does demonstrate the fact that when it comes to performance, functional programming isn't always the best solution.
+
+
+
+
+
+
---
+++ app/view/main/Main.js
@@ -0,0 +1,148 @@
+/**
+ * This class is the main view for the application. It is specified in app.js as the
+ * "mainView" property. That setting automatically applies the "viewport"
+ * plugin causing this view to become the body element (i.e., the viewport).
+ *
+ * TODO - Replace this content of this view to suite the needs of your application.
+ */
+Ext.define('Demo.view.main.Main', {
+    extend: 'Ext.panel.Panel',
+    xtype: 'app-main',
+
+    requires: [
+        'Ext.plugin.Viewport'
+
+    ],
+    bodyPadding: "40",
+    title: "Demo - Performance Compare between For Loop and Functional Programming via Array.map",
+    items: [{
+        xtype: "container",
+        html: "<h1>You can monitor result in the browser's console.<h1><h2>Each scenario resolves around going to embedded loops.  A major loop (preset to 10) in the code and all inner loops are based on a count of 10 items."
+    }, {
+        xtype: 'toolbar',
+        items: [{
+            xtype: "numberfield",
+            fieldLabel: "Type a value (beware, values beyond 100 may take a while to process) (no value defaults to 10)",
+            itemId: "limit",
+            labelWidth: 300,
+            defaultValue: 10,
+            value: 10
+        }, {
+            xtype: "button",
+            text: "Start Profiling",
+            listeners: {
+                click: {
+                    fn: function(btn) {
+                        let cmp = btn.up('app-main');
+                        let resultsCmp = cmp.down('container[itemId=results]');
+                        resultsCmp.setHtml("");
+
+                        window.setTimeout(function() {
+                            let numberField = btn.prev();
+                            let v = numberField.getValue();
+                            let defaultValue = numberField.defaultValue;
+                            v = (!v) ? defaultValue : v;
+
+                            if(v <= 0) {
+                                v = defaultValue;
+                                numberField.setValue(v);
+                            }
+
+                            let start1, end1, start2, end2, elapsed1, elapsed2;
+                
+                            let demoArrSize = v;
+                            let largeArr = (() => {
+                                
+                                let arr = [];
+                                for(x = 0; x < demoArrSize; x++) {
+                                    arr.push(`Number: ${x + 1}` );
+                                }
+                
+                                return arr;
+                            })();
+                
+                            let smallArr = (() => {
+                                
+                                let arr = [];
+                                for(x = 0; x < 10; x++) {
+                                    arr.push(`Inner Number: ${x + 1}` );
+                                }
+                
+                                return arr;
+                            })();
+                
+                
+                            console.log("Iterating via For Loop");
+                            console.log("Start");
+                            start1 = new Date();
+                            console.log(start1);
+                
+                            let largeArrLength = largeArr.length;
+                            
+                            for(let x = 0; x < largeArrLength; x++) {
+                                console.log(largeArr[x]);
+                                let smallArrLength = smallArr.length;
+                                for(y = 0; y < smallArrLength; y++) {
+                                    console.log(y);
+                                    for(z = 0; z < smallArrLength; z++) {
+                                        console.log(z);
+                                        for(i = 0; i < smallArrLength; i++) {
+                                            console.log(i);
+                                        }       
+                                    }   
+                                }
+                            }
+                
+                            end1 = new Date();
+                            console.log(end1);
+                            console.log("End");
+                
+                            elapsed1 = end1 - start1;
+                            console.log(`Elapsed Time is ${elapsed1}`);
+                            console.log("\n**********************\n")
+                
+                            console.log("Iterating Array.map and an arrow function");
+                            console.log("Start");
+                            start2 = new Date();
+                            console.log(start2);
+                            
+                            largeArr.map((item) => {
+                                console.log(item);
+                                smallArr.map((item) => {
+                                    console.log(item);
+                                    smallArr.map((item) => {
+                                        console.log(item);
+                                        smallArr.map((item) => console.log(item));
+                                    })
+                                });
+                            });
+                            end2 = new Date();
+                            console.log(end2);
+                            console.log("End");
+                            
+                            elapsed2 = end2 - start2;
+                            console.log(`Elapsed Time is ${elapsed2}`);
+                
+                            var msg = `<h2>Results</h2>
+                            <h3>Size of Outer Array is ${demoArrSize}</h3>
+                            <h3>3 embedded inner loops are each the size of 10 items<h3>
+                            <h4>For Loop</h4>
+                            <h5>Elapsed Time: ${elapsed1}</h5>
+                            <h4>Array Map with Arrow Function</h4>
+                            <h5>Elapsed Time: ${elapsed2}</h5>`
+                
+                            resultsCmp.setHtml(msg);
+                        }, 300);
+
+                        
+                    }
+                }
+            }
+        }]
+    }, {
+        xtype: "container",
+        itemId: "results",
+        html: ""
+    }]
+
+});
